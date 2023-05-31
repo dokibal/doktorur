5dec99378f748a87c7e3450a919be370b78d7e91
dokibal <doktor.balazs1@gmail.com>
2023-05-31 16:54:20 +0200

    Add first test API call

---
+++ src/Services/CryptoService.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const CRYPTO_API_BASE_URL = "https://agile-fjord-18513.herokuapp.com";
+
+class CryptoService{
+    
+    getMain(){
+        return axios.get(CRYPTO_API_BASE_URL);
+    }
+}
+
+export default CryptoService();
